99c87bd7cc1b9f15e9014c190c23d9ca4746a51f
jvbsl <jvsbl@gmx.de>
2020-08-11 20:36:54 +0200

    Do not try to include symlinks(doesn't work)

--- make_nuget.ps1
+++ make_nuget.ps1
@@ -32,17 +32,3 @@ $version = "$verMajor.$verMinor.$verPatch.$buildVersionResult"
 $nuspec = [System.IO.Path]::Combine($projectDir, ".\glfw-redist.csproj")
 
 dotnet pack $nuspec -c Release -p:VERSION="$version" -p:GLFW_VERSION="$GLFW_VERSION" -o ./artifacts
-
-New-item tmp/zippatch/runtimes/linux-x64/native -ItemType Directory -Force
-
-$glfw_symlinks=Dir tmp/src/build/src/ -Force -ErrorAction 'silentlycontinue' | Where { $_.Attributes -match "ReparsePoint" -And $_.Name -match "libglfw*"}
-foreach($s in $glfw_symlinks) {
-    cp -P --preserve=links $s.FullName tmp/zippatch/runtimes/linux-x64/native/
-}
-
-
-pushd tmp/zippatch
-
-zip --symlinks -gr ../../artifacts/opentoolkit.redist.glfw.*.nupkg .
-
-popd
